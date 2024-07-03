7f8a0be99dbf918b023fc9c02181dc8faba48099
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-03 17:06:49 +0700

    add r2dbc config for database populator

---
+++ balance_service/src/main/java/com/phincon/backend/bootcamp/marketplace/balance_service/config/R2dbcConfig.java
@@ -0,0 +1,33 @@
+package com.phincon.backend.bootcamp.marketplace.balance_service.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@EnableR2dbcRepositories
+@Configuration
+@Slf4j
+public class R2dbcConfig {
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        log.debug("Connection factory set: {}", connectionFactory);
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+
+        populator.addScripts(
+                new ClassPathResource("create_table.sql"));
+
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+}
