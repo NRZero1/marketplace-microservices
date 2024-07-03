0803d5bf799583e0972f41f739d961f760498faf
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-03 15:29:08 +0700

    add balance request

---
+++ dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/BalanceRequest.java
@@ -0,0 +1,12 @@
+package com.phincon.backend.bootcamp.marketplace.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BalanceRequest {
+    private long balance;
+}
