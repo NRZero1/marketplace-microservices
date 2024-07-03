a78c0c7044a4a5f3d498e7944d8ac97586a0b8c9
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-03 17:09:30 +0700

    Balance service

---
+++ balance_service/src/main/java/com/phincon/backend/bootcamp/marketplace/balance_service/service/BalanceService.java
@@ -0,0 +1,62 @@
+package com.phincon.backend.bootcamp.marketplace.balance_service.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phincon.backend.bootcamp.marketplace.balance_service.repository.BalanceRepository;
+import com.phincon.backend.bootcamp.marketplace.dto.BalanceRequest;
+import com.phincon.backend.bootcamp.marketplace.balance_service.exception.BalanceNotFoundException;
+import com.phincon.backend.bootcamp.marketplace.balance_service.model.Balance;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class BalanceService {
+    @Autowired
+    private BalanceRepository balanceRepository;
+
+    public Flux<Balance> getAll() {
+        return balanceRepository.findAll();
+    }
+
+    public Mono<Balance> getById(long id) {
+        return balanceRepository.findById(id).switchIfEmpty(
+                Mono.error(
+                        new BalanceNotFoundException(
+                                String.format(
+                                        "Balance not found with ID: ", id))));
+    }
+
+    public Mono<Balance> save(BalanceRequest request) {
+        return balanceRepository.save(Balance
+                .builder()
+                .balance(request.getBalance())
+                .build());
+    }
+
+    public Mono<Balance> update(long id, BalanceRequest request) {
+        Balance balance = new Balance();
+        return balanceRepository.findById(id)
+                .map(Optional::of)
+                .defaultIfEmpty(Optional.empty())
+                .flatMap(optionalProduct -> {
+                    if (optionalProduct.isPresent()) {
+                        balance.setId(id);
+                        balanceRepository.save(balance);
+                    }
+
+                    return Mono.empty();
+                });
+    }
+
+    public Mono<Void> delete(long id) {
+        return balanceRepository.deleteById(id);
+    }
+
+    public Mono<Void> deleteAll() {
+        return balanceRepository.deleteAll();
+    }
+}
