9208bf372fa7fc44056561a1e0bfdf6eb1283c9c
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-03 13:57:37 +0700

    Product Controller

---
+++ product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/controller/ProductController.java
@@ -0,0 +1,63 @@
+package com.phincon.backend.bootcamp.marketplace.product_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import com.phincon.backend.bootcamp.marketplace.dto.ProductRequest;
+import com.phincon.backend.bootcamp.marketplace.product_service.model.Product;
+import com.phincon.backend.bootcamp.marketplace.product_service.service.ProductService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Product> create(@RequestBody ProductRequest product) {
+        return productService.save(product);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Product> getById(@PathVariable long id) {
+        return productService.getById(id);
+    }
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<Product> getByName(@RequestParam("name") String name) {
+        return productService.getByName(name);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Product> update(@PathVariable long id, @RequestBody ProductRequest productRequest) {
+        return productService.update(id, productRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> delete(@PathVariable long id) {
+        return productService.delete(id);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteAll() {
+        return productService.deleteAll();
+    }
+}
