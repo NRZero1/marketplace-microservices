2a05891ef5073a59f55e622773d9f71796f4fc81
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-03 11:24:54 +0700

    init commit

---
+++ product_service/src/test/java/com/phincon/backend/bootcamp/marketplace/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phincon.backend.bootcamp.marketplace.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
