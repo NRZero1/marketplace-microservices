38c7150781aa5d7c8bb1861f46b7e83c6c2f8b1e
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-04 14:51:57 +0700

    sql script for order and order item table

---
+++ order_service/src/main/resources/create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    billing_address VARCHAR(255) NOT NULL,
+    customer_id BIGINT NOT NULL,
+    order_date TIMESTAMP CURRENT_TIMESTAMP,
+    order_status VARCHAR(255) NOT NULL,
+    payment_method VARCHAR(255) NOT NULL,
+    shipping_address VARCHAR(255) NOT NULL,
+    total_amount BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS order-item (
+    id SERIAL PRIMARY KEY,
+    price FLOAT(8) NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INTEGER NOT NULL,
+    order_id BIGINT NOT NULL
+);
