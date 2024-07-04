d438e08859bd6ded4f9e9fce0692376e77cb6ebc
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-04 15:10:44 +0700

    order and order item model

---
+++ order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/model/Order.java
@@ -0,0 +1,40 @@
+package com.phincon.backend.bootcamp.marketplace.order_service.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    private long id;
+
+    private String billingAddress;
+
+    @Column("customer_id")
+    private long customerId;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderDate;
+
+    @Column("order_status")
+    private String orderStatus;
+
+    @Column("payment_method")
+    private String paymentMethod;
+
+    @Column("shipping_address")
+    private String shippingAddress;
+
+    @Column("total_amount")
+    private double totalAmount;
+}
---
+++ order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/model/OrderItem.java
@@ -0,0 +1,28 @@
+package com.phincon.backend.bootcamp.marketplace.order_service.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("order_item")
+public class OrderItem {
+    @Id
+    private long id;
+
+    private double price;
+
+    @Column("product_id")
+    private long productid;
+
+    private int quantity;
+
+    @Column("order_id")
+    private long orderId;
+}
