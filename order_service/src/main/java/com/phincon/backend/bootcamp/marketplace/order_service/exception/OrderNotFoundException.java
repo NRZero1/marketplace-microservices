0e3b3921dfb3014d680254935c5f2a40d30ffdb5
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-04 17:51:42 +0700

    OrderNotFoundException

---
+++ order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/exception/OrderNotFoundException.java
@@ -0,0 +1,15 @@
+package com.phincon.backend.bootcamp.marketplace.order_service.exception;
+
+public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public OrderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
