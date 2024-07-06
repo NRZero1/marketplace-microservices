d31c9f292b891b42b293d72fc16c5005b1c754c1
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-06 21:09:55 +0700

    init orchastration

---
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/OrchestrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phincon.backend.bootcamp.marketplace.orchestration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrationServiceApplication.class, args);
+	}
+
+}
---
+++ orchestration_service/src/test/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/OrchestrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phincon.backend.bootcamp.marketplace.orchestration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
