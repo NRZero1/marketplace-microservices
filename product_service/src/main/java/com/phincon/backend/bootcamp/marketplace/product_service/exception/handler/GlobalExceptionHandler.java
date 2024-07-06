e919e3350a7e7d6b9b08bc37db2ab1b1bc0689eb
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-06 20:57:14 +0700

    add web exchange bind exception for getting error validation field

--- product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/exception/handler/GlobalExceptionHandler.java
+++ product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/exception/handler/GlobalExceptionHandler.java
@@ -1,17 +1,24 @@
 package com.phincon.backend.bootcamp.marketplace.product_service.exception.handler;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.phincon.backend.bootcamp.marketplace.dto.ErrorResponse;
 import com.phincon.backend.bootcamp.marketplace.product_service.exception.ProductNotFoundException;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(ProductNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -20,5 +27,19 @@ public class GlobalExceptionHandler {
                 new ErrorResponse(
                         HttpStatus.NOT_FOUND.value(), ex.getMessage()));
         return errResponse;
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(WebExchangeBindException ex) {
+        log.trace("Entering handleValidationExceptions()");
+        Map<String, String> errors = ex.getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        fieldError -> fieldError.getField(),
+                        fieldError -> fieldError.getDefaultMessage(),
+                        (existingValue, newValue) -> existingValue,
+                        HashMap::new));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
