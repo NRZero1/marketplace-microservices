d430a703803d0b36dc996d3d4ebe635f99dcce4a
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-06 20:53:17 +0700

    add default constraint for order date column

--- order_service/src/main/resources/create_table.sql
+++ order_service/src/main/resources/create_table.sql
@@ -2,14 +2,14 @@ CREATE TABLE IF NOT EXISTS orders (
     id SERIAL PRIMARY KEY,
     billing_address VARCHAR(255) NOT NULL,
     customer_id BIGINT NOT NULL,
-    order_date TIMESTAMP CURRENT_TIMESTAMP,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     order_status VARCHAR(255) NOT NULL,
     payment_method VARCHAR(255) NOT NULL,
     shipping_address VARCHAR(255) NOT NULL,
     total_amount FLOAT(8) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS order-item (
+CREATE TABLE IF NOT EXISTS order_item (
     id SERIAL PRIMARY KEY,
     price FLOAT(8) NOT NULL,
     product_id BIGINT NOT NULL,
