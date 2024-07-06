1bd765f55642c74efa6dafbbb707ab6a2e5d1f87
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-06 20:55:33 +0700

    extending r2dbc repository

--- order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/repository/OrderItemRepository.java
+++ order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/repository/OrderItemRepository.java
@@ -1,8 +1,11 @@
 package com.phincon.backend.bootcamp.marketplace.order_service.repository;
 
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
+import com.phincon.backend.bootcamp.marketplace.order_service.model.OrderItem;
+
 @Repository
-public interface OrderItemRepository {
+public interface OrderItemRepository extends R2dbcRepository<OrderItem, Long> {
 
 }
