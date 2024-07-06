bbdf9b4a2c5d4d7012b0f74b5e5cb1af75dd6fe6
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-06 20:56:10 +0700

    order controller

---
+++ order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/controller/OrderController.java
@@ -0,0 +1,65 @@
+package com.phincon.backend.bootcamp.marketplace.order_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.phincon.backend.bootcamp.marketplace.dto.OrderRequest;
+import com.phincon.backend.bootcamp.marketplace.dto.OrderResponse;
+import com.phincon.backend.bootcamp.marketplace.order_service.model.Order;
+import com.phincon.backend.bootcamp.marketplace.order_service.service.OrderService;
+
+import jakarta.validation.Valid;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<Order> getAll() {
+        return orderService.getAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<OrderResponse> create(@RequestBody @Valid OrderRequest order) {
+        return orderService.save(order);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Order> getById(@PathVariable long id) {
+        return orderService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Order> update(@PathVariable long id, @RequestBody OrderRequest orderRequest) {
+        return orderService.update(id, orderRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> delete(@PathVariable long id) {
+        return orderService.delete(id);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteAll() {
+        return orderService.deleteAll();
+    }
+}
