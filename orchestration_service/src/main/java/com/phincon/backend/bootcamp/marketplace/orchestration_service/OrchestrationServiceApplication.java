3ca4ba99a2b067947a53ca654f291c98a8ab4ea9
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-07 14:10:32 +0700

    add enable kafka annotation

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/OrchestrationServiceApplication.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/OrchestrationServiceApplication.java
@@ -2,10 +2,11 @@ package com.phincon.backend.bootcamp.marketplace.orchestration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class OrchestrationServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrchestrationServiceApplication.class, args);
 	}
