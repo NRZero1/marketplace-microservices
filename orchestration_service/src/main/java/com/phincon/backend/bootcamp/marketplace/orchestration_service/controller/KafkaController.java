d35024479673cd6051f9876b7bd0522be3132300
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-07 14:10:50 +0700

    Kafka controller

---
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/controller/KafkaController.java
@@ -0,0 +1,26 @@
+package com.phincon.backend.bootcamp.marketplace.orchestration_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.phincon.backend.bootcamp.marketplace.orchestration_service.dto.Test;
+import com.phincon.backend.bootcamp.marketplace.orchestration_service.service.ProducerService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/message")
+public class KafkaController {
+    @Autowired
+    private ProducerService producerService;
+
+    @PostMapping
+    public Mono<String> sendMessage(@RequestBody Test test) throws JsonProcessingException {
+        producerService.sendMessageJson(test);
+        return Mono.just("Message sent with message: " + test);
+    }
+}
