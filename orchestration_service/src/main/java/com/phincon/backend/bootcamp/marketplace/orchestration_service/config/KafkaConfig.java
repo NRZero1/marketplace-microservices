3bd015c04bd79336686c8c5f0143bf39e21be6ef
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-07 15:53:56 +0700

    change config from programatically approach to properties config

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/config/KafkaConfig.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/config/KafkaConfig.java
@@ -20,29 +20,33 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    @Bean
-    public ProducerFactory<String, String> kafkaProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
+    // @Bean
+    // public ProducerFactory<String, String> kafkaProducerFactory() {
+    // Map<String, Object> configProps = new HashMap<>();
+    // configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    // configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+    // StringSerializer.class);
+    // configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+    // StringSerializer.class);
+    // return new DefaultKafkaProducerFactory<>(configProps);
+    // }
 
-    @Bean
-    public ConsumerFactory<String, String> kafkaConsumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
+    // @Bean
+    // public ConsumerFactory<String, String> kafkaConsumerFactory() {
+    // Map<String, Object> props = new HashMap<>();
+    // props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    // props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+    // props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+    // StringDeserializer.class);
+    // props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+    // StringDeserializer.class);
+    // return new DefaultKafkaConsumerFactory<>(props);
+    // }
 
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(kafkaProducerFactory());
-    }
+    // @Bean
+    // public KafkaTemplate<String, String> kafkaTemplate() {
+    // return new KafkaTemplate<>(kafkaProducerFactory());
+    // }
 
     @Bean
     public NewTopic testTopic() {
