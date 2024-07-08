35eed03aaf65f3287340095b628250b0d8a6b9b9
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-08 09:22:40 +0700

    add logging and change defaultIfEmpty to switchIfEmpty and set product status

--- product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/service/ProductService.java
+++ product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/service/ProductService.java
@@ -4,17 +4,22 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.phincon.backend.bootcamp.marketplace.dto.ProductRequest;
+import com.phincon.backend.bootcamp.marketplace.dto.ProductStockResponse;
 import com.phincon.backend.bootcamp.marketplace.product_service.exception.ProductNotFoundException;
 import com.phincon.backend.bootcamp.marketplace.product_service.model.Product;
 import com.phincon.backend.bootcamp.marketplace.product_service.repository.ProductRepository;
+import com.phincon.backend.bootcamp.marketplace.service_enums.ProductStockStatus;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
@@ -28,7 +33,7 @@ public class ProductService {
                 Mono.error(
                         new ProductNotFoundException(
                                 String.format(
-                                        "Product not found with ID: ", id))));
+                                        "Product not found with id: %d", id))));
     }
 
     public Flux<Product> getByName(String name) {
@@ -36,7 +41,8 @@ public class ProductService {
                 .switchIfEmpty(
                         Mono.error(
                                 new ProductNotFoundException(
-                                        String.format("Product not found with name: %s", name))));
+                                        String.format(
+                                                "Product not found with name: %s", name))));
     }
 
     public Mono<Product> save(ProductRequest request) {
@@ -50,18 +56,26 @@ public class ProductService {
                 .imageUrl(request.getImageUrl())
                 .stockQuantity(request.getStockQuantity())
                 .updatedAt(LocalDateTime.now())
-                .build());
+                .build())
+                .doOnSuccess((response) -> {
+                    log.info("Product is saved with data: {}", response.toString());
+                });
     }
 
     public Mono<Product> update(long id, ProductRequest request) {
-        Product product = new Product();
         return productRepository.findById(id)
                 .map(Optional::of)
-                .defaultIfEmpty(Optional.empty())
+                .switchIfEmpty(
+                        Mono.error(new ProductNotFoundException(String.format("Product not found with id: %d", id))))
                 .flatMap(optionalProduct -> {
                     if (optionalProduct.isPresent()) {
+                        Product product = new Product();
                         product.setId(id);
-                        return productRepository.save(product);
+                        return productRepository.save(product)
+                                .doOnSuccess((response) -> {
+                                    log.info("Product is updated, current product data with id {} is {}", id,
+                                            response.toString());
+                                });
                     }
 
                     return Mono.empty();
@@ -76,16 +90,50 @@ public class ProductService {
         return productRepository.deleteAll();
     }
 
-    public Mono<Product> deduct(long id, int deducted, Product product) {
+    public Mono<ResponseEntity<ProductStockResponse>> deduct(long id, int deducted) {
         return productRepository.findById(id)
                 .map(Optional::of)
-                .defaultIfEmpty(Optional.empty())
+                .switchIfEmpty(
+                        Mono.error(new ProductNotFoundException(String.format("Product not found with id: %d", id))))
                 .flatMap(optionalProduct -> {
                     if (optionalProduct.isPresent()) {
-                        product.setStockQuantity(deducted);
-                        return productRepository.save(product);
+                        Product product = optionalProduct.get();
+                        int currentStock = product.getStockQuantity();
+                        log.debug("Current product with Id {} stock is {}", id, product.getStockQuantity());
+                        log.debug("Deducted parameter is set as: {}", deducted);
+
+                        if (currentStock < deducted) {
+                            log.error("Product with id {} not having enough stock", id);
+                            return Mono.just(ResponseEntity
+                                    .ok(mapToProductStockResponse(product, ProductStockStatus.OUTOFSTOCK.name())));
+
+                        } else if (currentStock == deducted) {
+                            log.warn("Product stock with id {} will be empty");
+                        }
+                        int deductedStock = currentStock - deducted;
+                        log.debug("Stock quantity after calculation: {}", deductedStock);
+                        product.setStockQuantity(deductedStock);
+                        return productRepository.save(product)
+                                .map((savedProduct) -> {
+                                    return ResponseEntity
+                                            .ok(mapToProductStockResponse(savedProduct, ProductStockStatus.OK.name()));
+                                })
+                                .doOnSuccess((response) -> {
+                                    log.info("Product stock updated with product data now: {}", response.toString());
+                                });
                     }
                     return Mono.empty();
                 });
+    }
+
+    public ProductStockResponse mapToProductStockResponse(Product product, String status) {
+        return ProductStockResponse
+                .builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .stockQuantity(product.getStockQuantity())
+                .status(status)
+                .build();
     }
 }
