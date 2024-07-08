14b0dd4d42c2c7a0d3fa32129e0802bd978d06ce
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-08 15:13:07 +0700

    web client config (base url)

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/config/WebClientConfig.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.phincon.backend.bootcamp.marketplace.orchestration_service.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,7 +8,26 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    @Qualifier("order")
+    public WebClient orderClient() {
+        return WebClient.builder()
+                .baseUrl("localhost:8084")
+                .build();
+    }
+
+    @Bean
+    @Qualifier("transaction")
+    public WebClient paymentClient() {
+        return WebClient.builder()
+                .baseUrl("localhost:8083")
+                .build();
+    }
+
+    @Bean
+    @Qualifier("product")
+    public WebClient productClient() {
+        return WebClient.builder()
+                .baseUrl("localhost:8081")
+                .build();
     }
 }
