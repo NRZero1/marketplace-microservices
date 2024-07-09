4f7c2000596133aa178ac5161b3284ea412cf994
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-09 09:54:46 +0700

    response for order failed

---
+++ dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Response/OrderFailedResponse.java
@@ -0,0 +1,36 @@
+package com.phincon.backend.bootcamp.marketplace.dto.Response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderFailedResponse {
+    private long id;
+
+    private String billingAddress;
+
+    private long customerId;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderDate;
+
+    private String orderStatus;
+
+    private String orderFailedMessage;
+
+    private String paymentMethod;
+
+    private String shippingAddress;
+
+    private List<OrderItemResponse> orderItem;
+
+    private double totalAmount;
+}
