7315b8308357330f2a9357f860bc89b2536cf0bc
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-09 20:11:12 +0700

    feat: all transaction method created and fixed

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/TransactionWebClient.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/TransactionWebClient.java
@@ -6,7 +6,10 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.phincon.backend.bootcamp.marketplace.dto.Response.TransactionResponse;
+import com.phincon.backend.bootcamp.marketplace.dto.request.TransactionRequest;
+import com.phincon.backend.bootcamp.marketplace.dto.response.BalanceResponse;
+import com.phincon.backend.bootcamp.marketplace.dto.request.BalanceRequest;
+import com.phincon.backend.bootcamp.marketplace.dto.response.TransactionResponse;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -18,15 +21,71 @@ public class TransactionWebClient {
     @Qualifier("transaction")
     private WebClient webClient;
 
-    public Mono<TransactionResponse> debit(long id) {
-        log.trace("Entering debit function");
-        String uriPath = String.format("localhost:8083/api/transaction");
+    public Mono<TransactionResponse> create(TransactionRequest transactionRequest) {
+        log.trace("Entering transaction create function");
+        String uriPath = String.format("api/transaction");
         Mono<TransactionResponse> result = this.webClient.post()
                 .uri((builder) -> builder.path(uriPath).build())
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(transactionRequest)
                 .retrieve()
-                .bodyToMono(TransactionResponse.class);
+                .bodyToMono(TransactionResponse.class)
+                .flatMap(rawResponse -> {
+                    log.info("Raw response is: {}", rawResponse);
+                    return Mono.just(rawResponse);
+                })
+                .doOnError(response -> log.error(response.getMessage()));
+        return result;
+    }
+
+    public Mono<BalanceResponse> checkBalance(long id) {
+        log.trace("Entering checkBalance function");
+        String uriPath = String.format("api/balance/%d", id);
+        Mono<BalanceResponse> result = this.webClient.get()
+                .uri((builder) -> builder.path(uriPath).build())
+                .retrieve()
+                .bodyToMono(BalanceResponse.class)
+                .flatMap(rawResponse -> {
+                    log.info("Raw response is: {}", rawResponse);
+                    return Mono.just(rawResponse);
+                });
+        return result;
+    }
+
+    public Mono<TransactionResponse> updateTransactionStatus(TransactionResponse transactionResponse) {
+        log.trace("Entering updateTransactionStatus function");
+        String uriPath = String.format("api/transaction/%d", transactionResponse.getId());
+        Mono<TransactionResponse> result = this.webClient.put()
+                .uri((builder) -> builder.path(uriPath).build())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(transactionResponse)
+                .retrieve()
+                .bodyToMono(TransactionResponse.class)
+                .flatMap(rawResponse -> {
+                    log.info("Raw response is: {}", rawResponse);
+                    return Mono.just(rawResponse);
+                });
+        return result;
+    }
+
+    public Mono<BalanceResponse> updateBalance(long id, BalanceRequest balanceRequest) {
+        log.trace("Entering updateBalance function");
+        String uriPath = String.format("api/balance/%d", id);
+        Mono<BalanceResponse> result = this.webClient.put()
+                .uri((builder) -> builder.path(uriPath)
+                        .build())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(balanceRequest)
+                .retrieve()
+                .bodyToMono(BalanceResponse.class)
+                .flatMap(rawResponse -> {
+                    log.info("Raw response is: {}", rawResponse);
+                    return Mono.just(rawResponse);
+                })
+                .doOnError(response -> log.error(response.getMessage()));
         return result;
     }
 }
