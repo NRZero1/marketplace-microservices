b33d63f390c2314298a92497dfb642c1bbaa8b02
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-09 09:52:46 +0700

    add webclient function handler for product stock rollback

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/ProductWebClient.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/ProductWebClient.java
@@ -9,7 +9,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.phincon.backend.bootcamp.marketplace.dto.Response.ProductStockResponse;
 
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -21,6 +20,18 @@ public class ProductWebClient {
 
     public Mono<ProductStockResponse> deductProduct(long id, int quantity) {
         log.trace("Entering deductProduct function");
+        String uriPath = String.format("localhost:8081/api/product/%d/deduct", id);
+        Mono<ProductStockResponse> result = this.webClient.patch()
+                .uri((builder) -> builder.path(uriPath).queryParam("amount", quantity).build())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(ProductStockResponse.class);
+        return result;
+    }
+
+    public Mono<ProductStockResponse> addStock(long id, int quantity) {
+        log.trace("Entering addStock function");
         String uriPath = String.format("localhost:8081/api/product/%d/deduct", id);
         Mono<ProductStockResponse> result = this.webClient.patch()
                 .uri((builder) -> builder.path(uriPath).queryParam("amount", quantity).build())
