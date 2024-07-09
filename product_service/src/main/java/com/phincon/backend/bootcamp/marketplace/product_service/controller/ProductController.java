994eeec0c5e8ad3db7cf3e31ead891214fdce3f6
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-09 20:02:21 +0700

    change annotation import from * to each individual used annotation also refactor dto package

--- product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/controller/ProductController.java
+++ product_service/src/main/java/com/phincon/backend/bootcamp/marketplace/product_service/controller/ProductController.java
@@ -3,18 +3,30 @@ package com.phincon.backend.bootcamp.marketplace.product_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.phincon.backend.bootcamp.marketplace.dto.Request.ProductRequest;
-import com.phincon.backend.bootcamp.marketplace.dto.Response.ProductStockResponse;
+import com.phincon.backend.bootcamp.marketplace.dto.request.ProductRequest;
+import com.phincon.backend.bootcamp.marketplace.dto.response.ProductStockResponse;
 import com.phincon.backend.bootcamp.marketplace.product_service.model.Product;
 import com.phincon.backend.bootcamp.marketplace.product_service.service.ProductService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/product")
 public class ProductController {
 
@@ -35,7 +47,7 @@ public class ProductController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Mono<Product> getById(@PathVariable long id) {
+    public Mono<ProductStockResponse> getById(@PathVariable long id) {
         return productService.getById(id);
     }
 
@@ -66,6 +78,7 @@ public class ProductController {
     @PatchMapping("/{id}/deduct")
     public Mono<ResponseEntity<ProductStockResponse>> deduct(@PathVariable long id,
             @RequestParam(name = "amount") int amount) {
+        log.trace("Entering deduct controller");
         return productService.deduct(id, amount);
     }
 
