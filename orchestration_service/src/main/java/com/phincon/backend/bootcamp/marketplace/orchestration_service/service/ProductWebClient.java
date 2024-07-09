3df25dbb4ade64ed9864d26855ea5e3dfea0fa48
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-09 20:11:51 +0700

    add logging, fix uriPath and add getProduct function

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/ProductWebClient.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/ProductWebClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.phincon.backend.bootcamp.marketplace.dto.Response.ProductStockResponse;
+import com.phincon.backend.bootcamp.marketplace.dto.response.ProductStockResponse;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -18,9 +18,27 @@ public class ProductWebClient {
     @Qualifier("product")
     private WebClient webClient;
 
-    public Mono<ProductStockResponse> deductProduct(long id, int quantity) {
+    public Mono<ProductStockResponse> deductProduct(Long id, int quantity) {
         log.trace("Entering deductProduct function");
-        String uriPath = String.format("localhost:8081/api/product/%d/deduct", id);
+        String uriPath = String.format("api/product/%s/deduct", id.toString());
+        log.info("value of uriPath is {}", uriPath);
+        Mono<ProductStockResponse> result = this.webClient.patch()
+                .uri((builder) -> builder.path(uriPath).queryParam("amount", quantity).build())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(ProductStockResponse.class)
+                .flatMap(rawResponse -> {
+                    log.info("Raw response is: {}", rawResponse);
+                    return Mono.just(rawResponse);
+                })
+                .doOnError(response -> log.error(response.getMessage()));
+        return result;
+    }
+
+    public Mono<ProductStockResponse> addStock(long id, int quantity) {
+        log.trace("Entering addStock function");
+        String uriPath = String.format("localhost:8081/api/product/%d/addStock", id);
         Mono<ProductStockResponse> result = this.webClient.patch()
                 .uri((builder) -> builder.path(uriPath).queryParam("amount", quantity).build())
                 .contentType(MediaType.APPLICATION_JSON)
@@ -30,13 +48,11 @@ public class ProductWebClient {
         return result;
     }
 
-    public Mono<ProductStockResponse> addStock(long id, int quantity) {
-        log.trace("Entering addStock function");
-        String uriPath = String.format("localhost:8081/api/product/%d/deduct", id);
-        Mono<ProductStockResponse> result = this.webClient.patch()
-                .uri((builder) -> builder.path(uriPath).queryParam("amount", quantity).build())
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
+    public Mono<ProductStockResponse> getProduct(long id) {
+        log.trace("Entering getProduct function");
+        String uriPath = String.format("localhost:8081/api/product/%d/", id);
+        Mono<ProductStockResponse> result = this.webClient.get()
+                .uri((builder) -> builder.path(uriPath).build())
                 .retrieve()
                 .bodyToMono(ProductStockResponse.class);
         return result;
