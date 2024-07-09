74203aa9d165f28a508e619b61ff1a7c6552af3c
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-09 10:06:28 +0700

    add customerId in transaction request dto

--- dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Request/TransactionRequest.java
+++ dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Request/TransactionRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionRequest {
+    private long customerId;
     private double amount;
     private long orderId;
     private String mode;
