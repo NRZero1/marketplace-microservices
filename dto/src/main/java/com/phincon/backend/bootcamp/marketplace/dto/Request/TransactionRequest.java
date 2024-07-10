f978b777b81193996d5a777d6e2916e23000257c
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-10 20:33:03 +0700

    add positive validation

--- dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Request/TransactionRequest.java
+++ dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Request/TransactionRequest.java
@@ -1,5 +1,8 @@
 package com.phincon.backend.bootcamp.marketplace.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class TransactionRequest {
+    @Positive(message = "customer id must be a positive integer")
     private long customerId;
+
+    @Positive(message = "amount must be a positive integer")
     private double amount;
+
+    @Positive(message = "order id must be a positive integer")
     private long orderId;
+
+    @NotBlank(message = "mode cannot be null or blank")
     private String mode;
     private String status;
     private String referenceNumber;
