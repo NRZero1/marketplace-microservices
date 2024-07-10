d603c86e94c6ccd35e6be882b34209dbf559f83e
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-10 20:33:58 +0700

    change accepted paramter from id and status to OrderResponse

--- orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/OrderWebClient.java
+++ orchestration_service/src/main/java/com/phincon/backend/bootcamp/marketplace/orchestration_service/service/OrderWebClient.java
@@ -1,16 +1,12 @@
 package com.phincon.backend.bootcamp.marketplace.orchestration_service.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.phincon.backend.bootcamp.marketplace.dto.Response.OrderResponse;
+import com.phincon.backend.bootcamp.marketplace.dto.response.OrderResponse;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -22,16 +18,14 @@ public class OrderWebClient {
     @Qualifier("order")
     private WebClient webClient;
 
-    public Mono<OrderResponse> updateStatus(long id, String status) {
+    public Mono<OrderResponse> updateStatus(OrderResponse orderResponse) {
         log.trace("Entering updateStatus function");
-        Map<String, String> bodyMap = new HashMap<>();
-        bodyMap.put("orderStatus", status);
-        String uriPath = String.format("localhost:8084/api/order/%d/updateStatus", id);
+        String uriPath = String.format("api/order/%d/updateStatus", orderResponse.getId());
         Mono<OrderResponse> result = this.webClient.patch()
                 .uri((builder) -> builder.path(uriPath).build())
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(bodyMap))
+                .bodyValue(orderResponse)
                 .retrieve()
                 .bodyToMono(OrderResponse.class);
         return result;
