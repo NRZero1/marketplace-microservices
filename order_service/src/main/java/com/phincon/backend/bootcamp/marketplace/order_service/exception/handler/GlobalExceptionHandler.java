a3b91ad8b252d450cf95873e65aadedfbfb68da5
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-10 20:23:46 +0700

    global exception handler

---
+++ order_service/src/main/java/com/phincon/backend/bootcamp/marketplace/order_service/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.phincon.backend.bootcamp.marketplace.order_service.exception.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import com.phincon.backend.bootcamp.marketplace.dto.response.ErrorResponse;
+import com.phincon.backend.bootcamp.marketplace.order_service.exception.OrderItemNotFoundException;
+import com.phincon.backend.bootcamp.marketplace.order_service.exception.OrderNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(OrderItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Mono<ErrorResponse> handleOrderItemNotFoundException(OrderItemNotFoundException ex) {
+        Mono<ErrorResponse> errResponse = Mono.just(
+                new ErrorResponse(
+                        HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+        return errResponse;
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Mono<ErrorResponse> handleOrderNotFoundException(OrderNotFoundException ex) {
+        Mono<ErrorResponse> errResponse = Mono.just(
+                new ErrorResponse(
+                        HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+        return errResponse;
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(WebExchangeBindException ex) {
+        log.trace("Entering handleValidationExceptions()");
+        Map<String, String> errors = ex.getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        fieldError -> fieldError.getField(),
+                        fieldError -> fieldError.getDefaultMessage(),
+                        (existingValue, newValue) -> existingValue,
+                        HashMap::new));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Mono<ErrorResponse> handleGeneralException(Exception ex) {
+        log.error("An unexpected error occurred: ", ex);
+        return Mono.just(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An unexpected error occurred"));
+    }
+}
