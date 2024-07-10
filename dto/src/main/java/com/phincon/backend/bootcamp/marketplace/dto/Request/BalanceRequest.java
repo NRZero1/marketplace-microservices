b3e4e2d69eca607cdd38e5b9b5c568d9917daa7d
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2024-07-10 20:24:25 +0700

    add validation

--- dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Request/BalanceRequest.java
+++ dto/src/main/java/com/phincon/backend/bootcamp/marketplace/dto/Request/BalanceRequest.java
@@ -1,5 +1,6 @@
 package com.phincon.backend.bootcamp.marketplace.dto.request;
 
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BalanceRequest {
+    @Min(value = 10000, message = "Min balance is 10000")
     private double balance;
 }
